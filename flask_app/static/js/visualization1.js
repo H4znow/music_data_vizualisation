4d611d6c2bdd0602ce146ae82268ed38c220d647
H4znow <haddouamine2002@gmail.com>
2024-10-21 11:17:42 +0200

    Add Flask routes and templates for music distribution visualizations

---
+++ flask_app/static/js/visualization1.js
@@ -0,0 +1,10 @@
+// Sample D3.js code for World Music Distribution
+d3.select("#visualization1")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500)
+  .append("circle")
+  .attr("cx", 250)
+  .attr("cy", 250)
+  .attr("r", 200)
+  .style("fill", "steelblue");
---
+++ flask_app/static/js/visualization2.js
@@ -0,0 +1,11 @@
+// Sample D3.js code for Genre Popularity
+d3.select("#visualization2")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500)
+  .append("rect")
+  .attr("x", 50)
+  .attr("y", 50)
+  .attr("width", 400)
+  .attr("height", 400)
+  .style("fill", "orange");
